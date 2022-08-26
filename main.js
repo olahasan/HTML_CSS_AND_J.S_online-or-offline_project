d41ee73a48a6d1fda0996a08d15bb842201b112c
olahasan <ola.ali1000@gmail.com>
2022-08-26 01:40:31 -0700

    Upload The Design

---
+++ main.js
@@ -0,0 +1,45 @@
+let title = document.querySelector(".title");
+let ul = document.querySelector("ul");
+let reload = document.querySelector(".reload");
+
+window.onload = function () {
+    if (window.navigator.onLine) {
+        online()
+    } else {
+        offline()
+    }
+}
+
+window.addEventListener("online", function () {
+    online()
+});
+
+window.addEventListener("offline", function () {
+    offline()
+});
+
+reload.onclick = function () {
+    window.location.reload()
+}
+
+function online() {
+    title.innerHTML = "online Now";
+    title.style.color = "green";
+    ul.classList.add("hide");
+    reload.classList.add("hide");
+}
+
+function offline() {
+    title.innerHTML = "offline Now";
+    title.style.color = "#666";
+    ul.classList.remove("hide");
+    reload.classList.remove("hide");
+}
+
+
+
+console.log(title);
+console.log(ul);
+console.log(reload);
+
+
